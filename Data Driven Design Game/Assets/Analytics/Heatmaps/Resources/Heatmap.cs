16039133be82aa6ac8d8f6e835268115baf176f5
Morgan Skillicorn <hello@morgan.games>
2020-10-20 21:54:38 +0100

    initial push

---
+++ Data Driven Design Game/Assets/Analytics/Heatmaps/Resources/Heatmap.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Heatmap_BombDrop : MonoBehaviour
+{
+    private static List<Vector2> m_deathPositions = new List<Vector2>();
+    private static GameObject heatmapPrefab;
+    private static string m_path = "Assets/Analytics/Data";
+
+#if UNITY_EDITOR
+    [MenuItem("Tools/Heatmap/Generate Death Heatmap")]
+    static void ReadDeathData()
+    {
+        m_deathPositions.Clear();
+
+        string filePath = m_path + "/" + SceneManager.GetActiveScene().name + " deaths";
+        heatmapPrefab = (GameObject)Resources.Load("prefabs/heatmapMarkerPrefab", typeof(GameObject));//Prefab to use to render death positions.
+
+        //Read the text from directly from the txt file
+        string fullPath = filePath + ".txt";
+        StreamReader reader = new StreamReader(fullPath);
+
+        reader = new StreamReader(fullPath);
+
+        string deathCoords = "";
+        while ((deathCoords = reader.ReadLine()) != null) {//going through the text file line by line and adding it to a list of vectors.
+            m_deathPositions.Add(stringToVec(deathCoords));
+            deathCoords = "";
+        }
+        reader.Close();
+       renderDeathData();
+    }
+#endif
+
+
+    public static Vector2 stringToVec(string _st)
+    {
+        Vector2 result = new Vector2();
+        string[] vals = _st.Split(',');
+        if (vals.Length == 2)
+        {
+            result.Set(float.Parse(vals[0]), float.Parse(vals[1]));
+        }
+        return result;
+    }
+
+    public static void renderDeathData()
+    {
+        foreach (Vector2 deathPos in m_deathPositions) {
+
+            Instantiate(heatmapPrefab, deathPos, Quaternion.identity);
+        }
+    }
+
+#if UNITY_EDITOR
+    [MenuItem("Tools/Heatmap/Clear")]
+    public static void destroyHeatmapObjects()
+    {
+            GameObject[] hMap_Spheres = GameObject.FindGameObjectsWithTag("heatmap");
+            for (int i = 0; i < hMap_Spheres.Length; i++)
+            {
+                GameObject.DestroyImmediate(hMap_Spheres[i]);
+            }
+    }
+#endif
+}
---
+++ Data Driven Design Game/Assets/Analytics/SendData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendData : MonoBehaviour
+{
+    public void Send()
+    {
+        Discord.SendFile(
+            "Level 1 death data file",
+            "Level 1 deaths.csv",
+            "csv",
+            "Assets/Analytics/Data/Level 1 deaths.txt",
+            "application/msexcel",
+            "Lvl-1 Death Data",
+            "https://discordapp.com/api/webhooks/767362643455377408/oaZPDUpITeSHH-p75uaeuEFs3iP3bhJ_mzq-kQ2K49NTImq7GC4HwtiZgLUC5XpXOd_v");
+
+        Application.Quit();
+    }
+}
